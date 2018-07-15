e1471e5487b32553a097b4cf1309381178c12717
Recursivex5137 <recursivex5137@gmail.com>
2018-07-14 22:51:09 -0400

    Adding basic components a SFC and a component with state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import './App.css';
 
+import Description from './components/description';
+import Header from './components/header';
+
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -11,9 +14,11 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React and TimeTracker</h1>
         </header>
+        <Header name="Matthew" />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Description countBy={3} />
       </div>
     );
   }
